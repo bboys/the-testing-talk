5ab64327cf2eab23307633f46bb7c46444feab95
Mark Redeman <markredeman@gmail.com>
2015-07-04 17:17:45 +0200

    Updated backslash try1 tests
    Conflicts:
    	examples/backslash/try1/backslash.m
    	examples/backslash/try1/test_backslash.m

--- examples/backslash/try1/test_backslash.m
+++ examples/backslash/try1/test_backslash.m
@@ -2,6 +2,14 @@ function tests = test_backslash
     tests = functiontests(localfunctions);
 end
 
-function test_small_matrix
-    assert(backslash([1,1; 1,-1], [1;2]) == [3, -1]);
+function test_small_matrix(t)
+    t.assertEqual(backslash([1,1; 1,-1], [1;2]), [1.5; - 0.5]);
+end
+
+function test_random_small_matrix(t)
+    A = rand(2);
+    x = rand(2, 1);
+    b = A * x;
+
+    t.assertEqual(backslash(A, x), b);
 end
