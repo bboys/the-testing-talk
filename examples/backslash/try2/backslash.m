545c87afb107809b8aa05979666b1bf0b9c5412f
Mark Redeman <markredeman@gmail.com>
2015-07-04 17:42:37 +0200

    Backslash solves systems with nonsingular 2x2 matrices

---
+++ examples/backslash/try2/backslash.m
@@ -0,0 +1,9 @@
+function x = backslash(A, b)
+    % test_errors_when_A_and_b_are_not_of_appropriate_size
+    % n = size(b, 1);
+    % assertEqual([n, n], size(A), 'Wrong matrix dimensions');
+
+    Ainv = [A(2,2), -A(1,2); -A(2,1), A(1,1)];
+    Ainv = (1 / (A(1, 1) * A(2, 2) - A(2, 1) * A(1, 2))) * Ainv;
+    x = Ainv * b;
+end
